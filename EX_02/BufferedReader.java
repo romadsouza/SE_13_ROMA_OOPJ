fca0effddc624ab8b44fd3b11057b29ca7af8069
ROMA DSOUZA <roma.235977201@vcet.edu.in>
2024-10-17 12:08:39 +0530

    Update BufferedReader.java

--- EX_02/BufferedReader.java
+++ EX_02/BufferedReader.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BufferedReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
         try {
